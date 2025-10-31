5fad454e0e372cf92e030c4603bcd07d7c14675b
Stephen Daunheimer <469447+stallmer@users.noreply.github.com>
2025-10-30 21:26:22 -0700

    add hyprland from solopasha copr

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,18 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y \
+	tmux \
+	rclone \
+	kitty \
+	neovim \
+	waybar \
+	tailscale \
+	wofi \
+	blueman \
+	network-manager-applet \
+	fontawesome-fonts-all \
+	brightnessctl
 
 # Use a COPR Example:
 #
@@ -18,6 +29,14 @@ dnf5 install -y tmux
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr enable solopasha/hyprland
+dnf5 -y install \
+	hyprland \
+	hyprpaper \
+	hypridle \
+	hyprlock \
+	hyprland-qtutils
+dnf5 -y copr disable solopasha/hyprland
 
 #### Example for enabling a System Unit File
 
